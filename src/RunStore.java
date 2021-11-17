745b68d1998f744ad4608c2944cd4df3dce06e42
khanh19 <khanh02092000@gmail.com>
2021-11-17 13:02:27 -0500

    Done RunStore

---
+++ src/RunStore.java
@@ -0,0 +1,129 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RunStore {
+    public class RunInfo {
+        private FileReader converter;
+        private int current;
+        private int beginOff;
+        private int closeOff;
+
+        public RunInfo() {
+            this.beginOff = -1;
+            this.current = beginOff;
+            this.closeOff = -1;
+        }
+
+        public RunInfo(int beginOff, int closeOff) {
+            this.beginOff = beginOff;
+            this.closeOff = closeOff;
+            this.current = beginOff;
+
+        }
+
+        public FileReader getReader() {
+            return this.converter;
+        }
+
+        public int getCurr() {
+            return this.current;
+        }
+
+        public int getCloseOff() {
+            return this.closeOff;
+        }
+
+        public int getBeginOff() {
+            return this.beginOff;
+        }
+
+        public void setReader(FileReader converter) {
+            this.converter = converter;
+        }
+
+        public byte[] extractRun() {
+            byte[] currBlock;
+            if (this.getCurr() == this.getCloseOff()) {
+                return null;
+            } else if (this.getCurr() + 8192 > this.getCloseOff()) {
+                int nSize = this.getCloseOff() - this.getCurr();
+                currBlock = new byte[nSize];
+                try {
+                    converter.parseRange(currBlock, current, closeOff);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                current = closeOff;
+            } else {
+                currBlock = new byte[8192];
+                int upper = current + 8192;
+                try {
+                    converter.parseRange(currBlock, current, upper);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                current = current + 8192;
+            }
+            return currBlock;
+        }
+
+    }
+
+    private ArrayList<RunInfo> runStore;
+    private String intput;
+
+    public RunStore(String input) {
+        runStore = new ArrayList<RunInfo>();
+        this.intput = input;
+    }
+
+    public ArrayList<RunInfo> getRunStore() {
+        return this.runStore;
+    }
+
+    public void setRunStore(ArrayList<RunInfo> runStore) {
+        this.runStore = runStore;
+    }
+
+    public String getIntput() {
+        return this.intput;
+    }
+
+    public void setIntput(String intput) {
+        this.intput = intput;
+    }
+
+    public ArrayList<RunInfo> getAllRun() throws IOException {
+        try {
+            FileReader convert = new FileReader(new File(this.getIntput()));
+            Record oldRecord = null;
+            int rPointer = 0;
+            int lPointer = 0;
+            byte[] currBlock;
+            while (convert.hasNext()) {
+                currBlock = convert.next();
+                for (int i = 0; i < currBlock.length; i += 16) {
+                    Record currRecord = new Record(Arrays.copyOfRange(currBlock, i, i + 16));
+                    if (oldRecord.compareTo(currRecord) > 0 && oldRecord != null) {
+                        runStore.add(new RunStore.RunInfo(lPointer, rPointer));
+                        lPointer = rPointer;
+                    }
+                    rPointer += 16;
+                    oldRecord = currRecord;
+                }
+            }
+            runStore.add(new RunStore.RunInfo(lPointer, rPointer));
+            lPointer = rPointer;
+            convert.closeFile();
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
+        return runStore;
+
+    }
+
+}
